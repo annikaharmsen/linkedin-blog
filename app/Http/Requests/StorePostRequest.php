c459b5404f07bee43023c860cbf370a4bd248435
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-07-25 17:38:21 +0200

    feat: create post controller index and store methods

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -11,7 +11,7 @@ class StorePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StorePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'author' => 'required|string',
+            'title' => 'required|string|unique:posts,title|max:255',
+            'body' => 'required|string'
         ];
     }
 }

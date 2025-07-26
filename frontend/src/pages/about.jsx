69527d3a76bb950615902526dde09ba89709ee57
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-07-26 14:57:24 +0200

    feat: create home and about pages, update site title

---
+++ frontend/src/pages/about.jsx
@@ -0,0 +1,9 @@
+export default function about() {
+    return (
+        <div>
+            <h1>About Page</h1>
+
+            <p>Welcome to the about page</p>
+        </div>
+    );
+}
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,9 @@
+export default function home() {
+    return (
+        <div>
+            <h1>Welcome to my Blog</h1>
+
+            <p>Check out my latest posts below!</p>
+        </div>
+    );
+}
